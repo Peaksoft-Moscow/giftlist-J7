ee644a4381cc96287bb2f639cdbb2685840cc174
timurlankasymbaev <moscow.peaksoft@gmail.com>
2023-12-16 17:28:30 +0300

    first commit

--- src/main/java/com/peakosoft/giftlistj7/GiftlistJ7Application.java
+++ src/main/java/com/peakosoft/giftlistj7/GiftlistJ7Application.java
@@ -1,13 +1,10 @@
-package com.example.giftlistj7;
+package com.peakosoft.giftlistj7;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @RestController
--- src/test/java/com/peakosoft/giftlistj7/GiftlistJ7ApplicationTests.java
+++ src/test/java/com/peakosoft/giftlistj7/GiftlistJ7ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.giftlistj7;
+package com.peakosoft.giftlistj7;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
