fccf98de237628e207640be67345e76e1f7159f0
ruslanbek1995 <151846352+ruslanbek1995@users.noreply.github.com>
2023-12-21 20:01:39 +0300

    sign-in done.

---
+++ src/main/java/com/giftlistj7/peakosoft/model/entities/Role.java
@@ -0,0 +1,37 @@
+package com.giftlistj7.peakosoft.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name +
+                '}';
+    }
+}
