42aa947201b1857e041340266028ce54a7e3d50e
ruslanbek1995 <151846352+ruslanbek1995@users.noreply.github.com>
2023-12-21 20:01:39 +0300

    sign-in done.

---
+++ src/main/java/com/giftlistj7/peakosoft/security/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.giftlistj7.peakosoft.security;
+
+import com.giftlistj7.peakosoft.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println(username);
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found by email: " + username));
+
+    }
+}
