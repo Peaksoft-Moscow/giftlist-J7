8bd655939e14ab2b0a987f9dc3c067faeaeaae36
NazgulMansuralieva <nazyu1994@gmail.com>
2023-12-25 15:26:37 +0300

    create entity

--- src/main/java/com/peakosoft/giftlistj7/model/entities/Category.java
+++ src/main/java/com/peakosoft/giftlistj7/model/entities/Category.java
@@ -1,24 +1,11 @@
 package com.peakosoft.giftlistj7.model.entities;
 
-import com.peakosoft.giftlistj7.model.enums.*;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.List;
-
-@Entity
-@Table(name = "categories")
-@Getter
-@Setter
-@NoArgsConstructor
 public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-
-    @OneToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH,CascadeType.PERSIST},mappedBy ="category")
-    private List<SubCategory> subCategories;
+    private Clothes clothes;
+    private Shoes shoes;
+    private HouseAndGarden houseAndGarden;
+    private SchoolRequirements schoolRequirements;
+    private Transport transport;
+    private List<Gift> gifts;
 }
