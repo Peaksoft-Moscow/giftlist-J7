61ed8315e7eb6cadefa13f8dc187e2a17e65bf23
BaikalAsanov <asanova.gtbaikal@gmail.com>
2023-12-27 14:30:09 +0300

    added wish-list

--- src/main/java/com/peakosoft/giftlistj7/controller/WishListController.java
+++ src/main/java/com/peakosoft/giftlistj7/controller/WishListController.java
@@ -1,9 +1,32 @@
 package com.peakosoft.giftlistj7.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.peakosoft.giftlistj7.model.dto.WishListRequest;
+import com.peakosoft.giftlistj7.model.dto.WishListResponse;
+import com.peakosoft.giftlistj7.service.WishListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping()
+@RequestMapping("api/WishLists")
+@RequiredArgsConstructor
 public class WishListController {
+    private final WishListService wishListService;
+    @PostMapping("/save")
+    public WishListResponse save(WishListRequest wishListRequest) {
+        return wishListService.save(wishListRequest);
+    }
+    @GetMapping()
+    public List<WishListResponse> findAllByUserId(Long id) {
+        return wishListService.findAll(id);
+    }
+    @GetMapping("/update/{id}")
+    public WishListResponse update(@PathVariable("id") Long id, WishListRequest wishListRequest) {
+        return wishListService.update(id, wishListRequest);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id){
+        wishListService.delete(id);
+    }
 }
