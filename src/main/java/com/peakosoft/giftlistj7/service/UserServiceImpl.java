f5e4b3c86f9e9b1dcf620f5bc99835a3fe8bed6e
ruslanbek1995 <151846352+ruslanbek1995@users.noreply.github.com>
2023-12-28 17:22:38 +0300

    changed files

--- src/main/java/com/peakosoft/giftlistj7/service/UserServiceImpl.java
+++ src/main/java/com/peakosoft/giftlistj7/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.peakosoft.giftlistj7.service;
 
-import com.giftlistj7.peakosoft.config.UserRepository;
+import com.peakosoft.giftlistj7.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
