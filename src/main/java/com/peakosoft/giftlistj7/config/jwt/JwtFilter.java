b2be8089b6b221f632f249070300279cadeb706d
ruslanbek1995 <151846352+ruslanbek1995@users.noreply.github.com>
2023-12-28 16:38:50 +0300

    changed files

--- src/main/java/com/peakosoft/giftlistj7/config/jwt/JwtFilter.java
+++ src/main/java/com/peakosoft/giftlistj7/config/jwt/JwtFilter.java
@@ -48,4 +48,5 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         filterChain.doFilter(request, response);
     }
+
 }
--- src/main/java/com/peakosoft/giftlistj7/config/jwt/JwtUtil.java
+++ src/main/java/com/peakosoft/giftlistj7/config/jwt/JwtUtil.java
@@ -1,6 +1,5 @@
 package com.peakosoft.giftlistj7.config.jwt;
 
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -15,48 +14,52 @@ import java.util.function.Function;
 
 @Service
 public class JwtUtil {
+
     @Value("java-7")
     private String secretKey;
     private final Long TOKEN_EXPIRATION = 24 * 7 * 60 * 60 * 1000L; // 7 days
 
-    public String createToken(Map<String,Object> claims,String subject){
+    public String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION))
-                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
-    public String generateToke(UserDetails userDetails){
-        Map<String,Object> claims = new HashMap<>();
-        return createToken(claims,userDetails.getUsername());
+    public String generateToke(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
     }
 
-    private Claims getAllClaimsFromToken(String token){
+    private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser()
                 .setSigningKey(secretKey)
                 .parseClaimsJws(token)
                 .getBody();
     }
 
-    private <T> T getClaimsFromToken(String token, Function<Claims,T>function){
+    private <T> T getClaimsFromToken(String token, Function<Claims, T> function) {
         final Claims claims = getAllClaimsFromToken(token);
         return function.apply(claims);
     }
 
-    public Date getExpirationDateToken(String token){
-        return getClaimsFromToken(token,Claims::getExpiration);
+    public Date getExpirationDateToken(String token) {
+        return getClaimsFromToken(token, Claims::getExpiration);
     }
-    private Boolean isTokenExpired(String token){
+
+    private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateToken(token);
         return expiration.before(new Date());
     }
-    public String getUsernameFromToken(String token){
-        return getClaimsFromToken(token,Claims::getSubject);
+
+    public String getUsernameFromToken(String token) {
+        return getClaimsFromToken(token, Claims::getSubject);
     }
-    public Boolean tokeIsValidation(String token, UserDetails userDetails){
+
+    public Boolean tokeIsValidation(String token, UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
