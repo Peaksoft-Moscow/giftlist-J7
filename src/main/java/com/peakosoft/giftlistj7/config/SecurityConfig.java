64389d8a1a21a684080b154ee8955288df3935c5
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2024-01-04 13:36:15 +0300

    last commit for today

--- src/main/java/com/peakosoft/giftlistj7/config/SecurityConfig.java
+++ src/main/java/com/peakosoft/giftlistj7/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -54,8 +53,6 @@ public class SecurityConfig {
                             .requestMatchers("/api/auth/sign-up").permitAll()
                             .anyRequest().authenticated();
                 })
-                .oauth2Client(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
