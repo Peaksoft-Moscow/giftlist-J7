4484ff17182c9d584e90777521e82ed346b33bef
ruslanbek1995 <151846352+ruslanbek1995@users.noreply.github.com>
2024-01-12 21:20:53 +0300

    booking проверка бронирование

--- src/main/java/com/peakosoft/giftlistj7/repository/BookingRepository.java
+++ src/main/java/com/peakosoft/giftlistj7/repository/BookingRepository.java
@@ -4,11 +4,14 @@ import com.peakosoft.giftlistj7.model.entities.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface BookingRepository extends JpaRepository<String,Long> {
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Long> {
     @Query("select boking from Booking boking where boking.name=:name")
     Optional<Booking> findByName(@Param("name") String name );
+
 }
 
--- src/main/java/com/peakosoft/giftlistj7/service/BookingService.java
+++ src/main/java/com/peakosoft/giftlistj7/service/BookingService.java
@@ -1,12 +1,16 @@
 package com.peakosoft.giftlistj7.service;
 
 
+import com.peakosoft.giftlistj7.model.entities.Booking;
+import com.peakosoft.giftlistj7.model.entities.User;
 import com.peakosoft.giftlistj7.repository.BookingRepository;
 import com.peakosoft.giftlistj7.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.util.Optional;
 
 
 @Service
@@ -16,5 +20,26 @@ public class BookingService {
     private final BookingRepository bookingRepository;
 
     public boolean book(Principal principal, Long bookingId) {
+        Optional<User> userOptional = userRepository.findByEmail(principal.getName());
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            // Проверяем, что бронирование еще не привязано к пользователю
+           Optional<Booking> bookingOptional = Optional.ofNullable(bookingRepository.findById(bookingId)
+                   .orElseThrow(() -> new EntityNotFoundException("booking whizh id not found")));
+            if (bookingOptional.isPresent()) {
+                Booking booking = bookingOptional.get();
+                if (!user.getBooking().contains(booking)) {
+                    user.getBooking().add(booking);
+                    booking.setUser(user);
+                    // Сохраняем изменения
+                    userRepository.save(user);
+                    bookingRepository.save(booking);
+                    return true;
+
+                }
+            }
+        }
+        return false;
+
     }
 }
