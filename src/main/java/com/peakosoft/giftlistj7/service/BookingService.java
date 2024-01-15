fdc5ce1e6b09fcafddd855a1126a4844c1376e27
ruslanbek1995 <151846352+ruslanbek1995@users.noreply.github.com>
2024-01-15 19:26:25 +0300

    booking res

--- src/main/java/com/peakosoft/giftlistj7/service/BookingService.java
+++ src/main/java/com/peakosoft/giftlistj7/service/BookingService.java
@@ -19,27 +19,52 @@ public class BookingService {
     private final UserRepository userRepository;
     private final BookingRepository bookingRepository;
 
-    public boolean book(Principal principal, Long bookingId) {
+    public User book(Principal principal, Long bookingId) {
         Optional<User> userOptional = userRepository.findByEmail(principal.getName());
         if (userOptional.isPresent()) {
             User user = userOptional.get();
+
             // Проверяем, что бронирование еще не привязано к пользователю
-           Optional<Booking> bookingOptional = Optional.ofNullable(bookingRepository.findById(bookingId)
-                   .orElseThrow(() -> new EntityNotFoundException("booking whizh id not found")));
+            Optional<Booking> bookingOptional = Optional.ofNullable(bookingRepository.findById(bookingId)
+                    .orElseThrow(() -> new EntityNotFoundException("booking with id not found")));
             if (bookingOptional.isPresent()) {
                 Booking booking = bookingOptional.get();
+
                 if (!user.getBooking().contains(booking)) {
                     user.getBooking().add(booking);
                     booking.setUser(user);
                     // Сохраняем изменения
                     userRepository.save(user);
                     bookingRepository.save(booking);
-                    return true;
-
+                    return user;
                 }
             }
         }
-        return false;
+        return null;
+    }
+    // удаление забронированного Usera
+    public User cancelBooking(Principal principal,Long bookingId){
 
+        Optional<User> userOptional =userRepository.findByEmail(principal.getName());
+        if (userOptional.isPresent()){
+            User user =userOptional.get();
+
+            Optional<Booking>bookingOptional= bookingRepository.findById(bookingId);
+            if (bookingOptional.isPresent()){
+                Booking booking = bookingOptional.get();
+
+                if (user.getBooking().contains(booking)){
+                    user.getBooking().remove(booking);
+                    booking.setUser(null);
+                    userRepository.save(user);
+                    bookingRepository.save(booking);
+
+                    return user;
+
+                }
+            }
+
+        }
+        return null;
     }
 }
