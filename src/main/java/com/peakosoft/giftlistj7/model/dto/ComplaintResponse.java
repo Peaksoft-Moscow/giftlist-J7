07c89fbd6e2145c46cb3eb0818f150c9170310d0
BaikalAsanov <asanova.gtbaikal@gmail.com>
2024-01-24 19:15:37 +0300

    Complaint started 2

--- src/main/java/com/peakosoft/giftlistj7/model/dto/ComplaintResponse.java
+++ src/main/java/com/peakosoft/giftlistj7/model/dto/ComplaintResponse.java
@@ -1,9 +1,17 @@
 package com.peakosoft.giftlistj7.model.dto;
 
+import com.peakosoft.giftlistj7.model.enums.Complaints;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class ComplaintResponse {
+    private Long id;
+    private Complaints complaints;
+    private String description;
+    private LocalDate createDate;
+
 }
