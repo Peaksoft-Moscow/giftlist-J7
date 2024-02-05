c5fd6495b1ec6fb482417b63767c58d6485bc2e1
NazgulMansuralieva <nazyu1994@gmail.com>
2024-02-05 19:25:16 +0300

    swagger222

--- src/main/java/com/peakosoft/giftlistj7/config/SecurityConfig.java
+++ src/main/java/com/peakosoft/giftlistj7/config/SecurityConfig.java
@@ -60,6 +60,9 @@ public class SecurityConfig {
                             .requestMatchers("/api/wish_lists/**").hasAnyAuthority("ADMIN","USER")
                             .requestMatchers("/api/friends/**").hasAnyAuthority("ADMIN","USER")
                             .requestMatchers("/api/charity/**").hasAnyAuthority("ADMIN","USER")
+                            .requestMatchers("/swagger-ui/**",
+                                    "/swagger-resources/*," +
+                                            "/v3/api-docs/**").permitAll()
                             .anyRequest().authenticated();
                 })
                 .oauth2Login(withDefaults())
