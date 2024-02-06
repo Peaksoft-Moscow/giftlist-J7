3195259b346d07e4affc766e23f67bb0e8b4a688
NazgulMansuralieva <nazyu1994@gmail.com>
2024-02-06 21:05:53 +0300

    swagger333

--- src/main/java/com/peakosoft/giftlistj7/config/SwaggerConfig.java
+++ src/main/java/com/peakosoft/giftlistj7/config/SwaggerConfig.java
@@ -16,6 +16,7 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
 import java.util.Collections;
 import java.util.List;
 
